6a87305be93b52c9ecd1bb540b1eb869c0fd9562
goldprogrammer123 <goldenhaule899@gmail.com>
2026-01-10 23:25:26 +0300

    . jsx was added at landing page

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import FeatureCard from '../FeatureCard/FeatureCard';
+import FeatureCard from '../FeatureCard/FeatureCard.jsx';
 import './LandingPage.css';
 import { useLanguage } from '../../context/LanguageContext.jsx';
 
